2b023b40c8725568d6058cb96e418e2021d8c8c3
D3VSJ <D3VSJ@protonmail.com>
2020-11-09 20:51:52 +0000

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,128 @@
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/D3VSJ/BetterDiscordPanel">
+    <img src="img/logo.png" alt="Logo" width="128" height="128">
+  </a>
+
+  <h1 align="center">StickyNotes</h1>
+
+  <p align="center">
+    Organize tasks efficiently with StickyNotes!
+    <br />
+    <br />
+    <a href="https://github.com/D3VSJ/StickyNotes/issues/new?assignees=&labels=Bug&template=bug_report.md&title=%5BBUG%5D">Report Bug</a>
+    ·
+    <a href="https://github.com/D3VSJ/StickyNotes/issues/new?assignees=&labels=Suggestions&template=suggestions.md&title=%5BSUGGESTION%5D">Request Feature</a>
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+* [Features](#features)
+* [Getting Started](#getting-started)
+* [Installation](#installation)
+* [Roadmap](#roadmap)
+* [Contributing](#contributing)
+* [License](#license)
+* [Built With](#built-with)
+* [Contact](#contact)
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<img src="img/previews/preview.png" alt="Logo" width="100%" height="100%">
+
+**StickyNotes** is an innovative web application made with Firebase to help you organize your tasks.
+
+## Features
+
+**Current Features:**
+* Login to your personal account
+* Signup with a new Account
+* Add new StickyNote
+* All StickyNotes saved into unique stores in Realtime Database
+
+**Coming Soon:**
+* Delete StickyNotes
+* More StickyNote Personalization
+* Settings
+
+## Getting Started
+
+1. Clone the repo
+```sh
+git clone https://github.com/D3VSJ/StickyNotes
+```
+2. Open ```index.html``` to open StickyNotes.
+
+3. Create an account or login to your account.
+
+4. Start using StickyNotes!
+
+## Installation
+
+1. Clone the repo
+```sh
+git clone https://github.com/D3VSJ/StickyNotes
+```
+2. Open <a href="https://firebase.google.com" target="blank">Firebase Console</a> and open console with a new created Firebase Project.
+3. Obtain your Firebase app credentials and replace with the credentials located in ```js/index.js```
+```
+  var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+  firebase.initializeApp(config);
+```
+4. Go to the Authentication section of your project and enable Email/Password Signin as a sign up method.
+5. Go to the Realtime Database section of your project and create a new database in test mode.
+6. Open StickyNotes, create an account and start organizing tasks!
+<!-- ROADMAP -->
+## Roadmap
+
+See the [open issues](https://github.com/D3VSJ/StickyNotes/issues) for a list of proposed features (and known issues).
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+### Built With
+
+* [jQuery](https://jquery.com)
+* [Firebase](https://firebase.google.com)
+
+---
+
+<div align="center">
+
+## Contact 
+
+Website : [sanjaysunil.me](https://sanjaysunil.me) &nbsp;&middot;&nbsp;
+Email : [D3VSJ@protonmail.com](mailto:D3VSJS@protonmail.com) &nbsp;&middot;&nbsp;
+Discord : [D3VSJ#9496](https://discordapp.com/users/732336924559278181)
+
+</div>
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,270 @@
+/**
+ * Author: Sanjay Sunil (D3VSJ)
+ * License: MIT
+ */
+
+$(document).ready(function () {
+  //initialize the firebase app
+  var config = {
+    apiKey: "AIzaSyBOcSWO7my0qriZBdWywWZPWSinkru71SM",
+    authDomain: "fire-stickynotes.firebaseapp.com",
+    databaseURL: "https://fire-stickynotes.firebaseio.com",
+    projectId: "fire-stickynotes",
+    storageBucket: "fire-stickynotes.appspot.com",
+    messagingSenderId: "450114125909",
+    appId: "1:450114125909:web:e51437caa132b3f78054dc"
+  };
+  firebase.initializeApp(config);
+
+  // Firebase References
+  var Auth = firebase.auth();
+  var dbRef = firebase.database();
+  var notesRef = dbRef.ref("notes");
+  var usersRef = dbRef.ref("users");
+  var auth = null;
+
+  //Register
+  $("#registerForm").on("submit", function (e) {
+    e.preventDefault();
+    var data = {
+      email: $("#registerEmail").val(), //get the email from Form
+      firstName: $("#registerFirstName").val(), // get firstName
+      lastName: $("#registerLastName").val(), // get lastName
+    };
+    var passwords = {
+      password: $("#registerPassword").val(), //get the pass from Form
+      cPassword: $("#registerConfirmPassword").val(), //get the confirmPass from Form
+    };
+    if (
+      data.email != "" &&
+      passwords.password != "" &&
+      passwords.cPassword != ""
+    ) {
+      if (passwords.password == passwords.cPassword) {
+        //create the user
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(data.email, passwords.password)
+          /*
+            .then(function (user) {
+              return user.updateProfile({
+                displayName: data.firstName + ' ' + data.lastName
+              })
+            })
+            */
+          .then(function (user) {
+            //now user is needed to be logged in to save data
+            auth = user;
+            //now saving the profile data
+            usersRef
+              .child(user.uid)
+              .set(data)
+              .then(function () {
+                console.log("User Information Saved:", user.uid);
+              });
+            console.log("Success");
+            window.location.reload();
+          })
+          .catch(function (error) {
+            console.log("Error creating user:", error);
+            new Noty({
+              type: 'error',
+              theme: "nest",
+              closeWith: ['button'],
+              text: error.message,
+              timeout: 5000,
+              progressBar: true
+            }).show()
+          });
+      } else {
+        //password and confirm password didn't match
+        console.log("ERROR: Passwords didn't match");
+      }
+    }
+  });
+
+  //Login
+  $("#loginForm").on("submit", function (e) {
+    e.preventDefault();
+
+    if ($("#loginEmail").val() != "" && $("#loginPassword").val() != "") {
+      //login the user
+      var data = {
+        email: $("#loginEmail").val(),
+        password: $("#loginPassword").val(),
+      };
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(data.email, data.password)
+        .then(function (authData) {
+          auth = authData;
+          window.location.reload();
+        })
+        .catch(function (error) {
+          console.log("Login Failed!");
+          console.log(error.message)
+          new Noty({
+            type: 'error',
+            theme: "nest",
+            closeWith: ['button'],
+            text: error.message,
+            timeout: 5000,
+            progressBar: true
+          }).show()
+        });
+    }
+  });
+
+  $("#logout").on("click", function (e) {
+    e.preventDefault();
+    console.log("Successfully logged out.")
+    firebase.auth().signOut();
+  });
+
+  //save createNote
+  $("#createNote").on("submit", function (event) {
+    event.preventDefault();
+    if (auth != null) {
+      if ($("#name").val() != "" || $("#email").val() != "") {
+        let current_datetime = new Date()
+        let formatted_date =  current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds() 
+        notesRef.child(auth.uid).push({
+          name: $("#name").val(),
+          value: $("#value").val(),
+          timestamp: formatted_date
+          /*
+          spec: {
+            date: $("#date").val(),
+            time: $("#time").val()
+          },
+          */
+        });
+        new Noty({
+          type: 'success',
+          theme: "nest",
+          closeWith: ['button'],
+          text: "Sticky Note Added!",
+          timeout: 5000,
+          progressBar: true
+        }).show()
+        document.createNote.reset();
+        $('#createNoteModal').modal('hide')
+      } else {
+        new Noty({
+          type: 'success',
+          theme: "nest",
+          closeWith: ['button'],
+          text: "Please fill at least a title or description!",
+          timeout: 5000,
+          progressBar: true
+        }).show()      }
+    } else {
+      //inform user to login
+    }
+  });
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is logged in
+      console.log("User is logged in:")
+      auth = user;
+      $("body").removeClass("auth-false").addClass("auth-true");
+      usersRef
+        .child(user.uid)
+        .once("value")
+        .then(function (data) {
+          var info = data.val();
+          if (user.photoUrl) {
+            $(".user-info img").show();
+            $(".user-info img").attr("src", user.photoUrl);
+            $(".user-info .user-name").hide();
+          } else if (user.displayName) {
+            $(".user-info img").hide();
+            $(".user-info").append(
+              '<span class="user-name">' + user.displayName + "</span>"
+            );
+            console.log("Welcome, " + user.displayName + "!");
+            new Noty({
+              type: 'success',
+              theme: "nest",
+              closeWith: ['button'],
+              text: "Welcome, " + user.displayName + "!",
+              timeout: 5000,
+              progressBar: true
+            }).show()
+          } else if (info.firstName) {
+            $(".user-info img").hide();
+            $(".user-info").append(
+              '<span class="user-name"> Hello, ' + info.firstName + "!</span>"
+            );
+            console.log("Welcome, " + info.firstName + "!");
+            new Noty({
+              type: 'success',
+              theme: "nest",
+              closeWith: ['button'],
+              text: "Welcome, " + info.firstName + "!",
+              timeout: 5000,
+              progressBar: true
+            }).show()
+          }
+        });
+        notesRef.child(user.uid).on("child_added", onChildAdd);
+    } else {
+      // No user is signed in.
+      $("body").removeClass("auth-true").addClass("auth-false");
+      auth && notesRef.child(auth.uid).off("child_added", onChildAdd);
+      $("#notes").html("");
+      auth = null;
+      //$("#loginForm").modal('show')
+      console.log("No user is logged in.")
+    }
+  });
+});
+
+function onChildAdd(snap) {
+  $("#notes").append(noteFormObject(snap.key, snap.val()));
+}
+
+//prepare createNote object's HTML
+function noteFormObject(key, notes) {
+  const fireNote = "StickyNote"
+
+  return (
+    '<div class="card text-white bg-secondary mb-3" style="min-width: 18rem; max-width: 18rem;">' +
+        '<div class="card-header">' +
+            fireNote +
+        "</div>" +
+        '<div class="card-body">' +
+            '<h5 class="card-title">' +
+                notes.name +
+            "</h5>" +
+            '<p class="card-text">' +
+                notes.value +
+            '</p>'+
+        "</div>" +
+        '<div class="card-footer">' +
+          '<small>' +
+            "Edited at: " + notes.timestamp +
+          '</small>' +
+        '</div>' +
+    "</div>"
+  );
+}
+
+function spanText(textStr, textClasses) {
+  var classNames = textClasses.map((c) => "text-" + c).join(" ");
+  return '<span class="' + classNames + '">' + textStr + "</span>";
+}
+
+function errorNotification() {
+  new Noty({
+    type: 'error',
+    theme: "nest",
+    closeWith: ['button'],
+    text: error.message,
+    timeout: 5000,
+    progressBar: true
+  }).show()
+}
+
+
