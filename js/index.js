9664952cff70140a47b9e1df9f9dfa1db6f8a1cd
Sanjay Sunil <sanjaysunil@protonmail.com>
2021-02-28 18:40:20 +0000

    Add firebase database url.

--- js/index.js
+++ js/index.js
@@ -12,7 +12,8 @@ $(document).ready(function () {
     storageBucket: "stickynotes-474d9.appspot.com",
     messagingSenderId: "472523983888",
     appId: "1:472523983888:web:b56b514654a96775dc4e95",
-    measurementId: "G-P9SVKC7620"
+    measurementId: "G-P9SVKC7620",
+    databaseURL: "https://stickynotes-474d9-default-rtdb.firebaseio.com/",
   };
   firebase.initializeApp(config);
 
